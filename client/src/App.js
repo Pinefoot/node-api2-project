605f79457e503f6a4295775fe102f8f5df3f7062
Pinefoot <jameslcarpino@gmail.com>
2020-05-05 22:37:10 -0700

    react app up and running with I believe cors working. styling is proving to be a head ache.

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,39 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
+
 import './App.css';
-import axios from 'axios'
+import axios from 'axios';
+import styled from 'styled-components';
 
-
-
+const QuoteCard = styled.div`
+  display: flex;
+  justify-content: space-evenly;
+  align-items: center;
+`
 
 function App() {
+  const [quoteData, setQuoteData] = useState([]);
+
+  useEffect(()=>{
+    axios.get('http://localhost:5000/api/posts')
+    .then(res =>setQuoteData(res.data))
+    .catch(err=>(err))
+
+  })
+
+
   return (
     <div className="App">
+      <h2>Quotes and stuff!</h2>
+      <QuoteCard>
+        {quoteData.map(data=>{
+          return (
+            <quoteCard>
+          <h3>Quote: {data.title}</h3>
+          <p>Content: {data.contents} </p>
+          </quoteCard>
+          )
+        })}
+      </QuoteCard>
       
     </div>
   );
--- index.js
+++ index.js
@@ -2,18 +2,32 @@ const express = require('express');
 const postsRouter = require('./posts/postsRouter.js');
 //const commentRouter = require('./posts/commentRouter.js'); //for the routing not sure if its posts yet.
 const server = express();
+const cors = require('cors')
+
+
+
 
 server.use(express.json());
+server.use(cors());
 
-server.get('/', (req, res)=>{
+server.get('/:id', cors(), function (req, res, next) {
+    res.json({msg: 'This is CORS-enabled for a Single Route'})
+  })
+
+
+
+
+server.get('/', cors(), (req, res, next)=>{
+    
     res.json({ query: req.query, params: req.params, headers: req.headers})
     res.send(`
     <h2>Server Place Holder title</h2>
     <p>Welcome to this Api</p>
     `)
+    res.json({msg: 'this is CORS-enabled'})
 })
 
-server.use('/api/posts', postsRouter);
+server.use('/api/posts',  postsRouter);
 
 server.listen(5000, ()=>{
     console.log('\n*** Server running on http://localhost:5000***\n')
